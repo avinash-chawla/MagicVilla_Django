5bc56a4a4db1e8fe56b2873c199ea54b4cbf39be
avinashchawla96 <avinashchawla96@gmail.com>
2020-07-28 21:10:12 +0530

    Custom Admin

--- MagicVilla_Django/settings.py
+++ MagicVilla_Django/settings.py
@@ -81,8 +81,11 @@ WSGI_APPLICATION = 'MagicVilla_Django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME' : 'btredb',
+        'USER' : 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost'
     }
 }
 
@@ -128,3 +131,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'MagicVilla_Django/static')
 ]
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
